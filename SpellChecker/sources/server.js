a30137bb7b0b5eacdd7c94581ac1ecb58dcce3bf
tdsparrow <sqallowlee@gmail.com>
2020-03-24 16:12:07 +0800

    add spell check info for chinese

--- SpellChecker/sources/server.js
+++ SpellChecker/sources/server.js
@@ -30,104 +30,125 @@
  *
  */
 
-'use strict';
+"use strict";
 
-const cluster = require('cluster');
-const config = require('config').get('SpellChecker');
+const cluster = require("cluster");
+const config = require("config").get("SpellChecker");
 
 //process.env.NODE_ENV = config.get('server.mode');
 
-const logger = require('./../../Common/sources/logger');
+const logger = require("./../../Common/sources/logger");
 
-const c_nCheckHealth = 60000, c_sCheckWord = 'color', c_sCheckLang = 1033;
-let idCheckInterval, canStartCheck = true;
+const c_nCheckHealth = 60000,
+  c_sCheckWord = "color",
+  c_sCheckLang = 1033;
+let idCheckInterval,
+  canStartCheck = true;
 let statusCheckHealth = true;
-function checkHealth (worker) {
-	logger.info('checkHealth');
-	if (!statusCheckHealth) {
-		logger.error('error check health, restart!');
-		worker.kill();
-		return;
-	}
-	worker.send({type: 'spell'});
-	statusCheckHealth = false;
+function checkHealth(worker) {
+  logger.info("checkHealth");
+  if (!statusCheckHealth) {
+    logger.error("error check health, restart!");
+    worker.kill();
+    return;
+  }
+  worker.send({ type: "spell" });
+  statusCheckHealth = false;
 }
-function endCheckHealth (msg) {
-	logger.info('endCheckHealth');
-	statusCheckHealth = true;
+function endCheckHealth(msg) {
+  logger.info("endCheckHealth");
+  statusCheckHealth = true;
 }
 
-const workersCount = 1;	// ToDo Пока только 1 процесс будем задействовать. Но в будующем стоит рассмотреть несколько.
+const workersCount = 1; // ToDo Пока только 1 процесс будем задействовать. Но в будующем стоит рассмотреть несколько.
 if (cluster.isMaster) {
-	logger.warn('start cluster with %s workers', workersCount);
-	cluster.on('listening', function(worker) {
-		if (canStartCheck) {
-			canStartCheck = false;
-			idCheckInterval = setInterval(function(){checkHealth(worker);}, c_nCheckHealth);
-			worker.on('message', function(msg){endCheckHealth(msg);});
-		}
-	});
-	for (let nIndexWorker = 0; nIndexWorker < workersCount; ++nIndexWorker) {
-		logger.warn('worker %s started.', cluster.fork().process.pid);
-	}
+  logger.warn("start cluster with %s workers", workersCount);
+  cluster.on("listening", function(worker) {
+    if (canStartCheck) {
+      canStartCheck = false;
+      idCheckInterval = setInterval(function() {
+        checkHealth(worker);
+      }, c_nCheckHealth);
+      worker.on("message", function(msg) {
+        endCheckHealth(msg);
+      });
+    }
+  });
+  for (let nIndexWorker = 0; nIndexWorker < workersCount; ++nIndexWorker) {
+    logger.warn("worker %s started.", cluster.fork().process.pid);
+  }
 
-	cluster.on('exit', (worker, code, signal) => {
-		logger.warn('worker %s died (code = %s; signal = %s). restart...', worker.process.pid, code, signal);
-		clearInterval(idCheckInterval);
-		endCheckHealth();
-		canStartCheck = true;
-		cluster.fork();
-	});
+  cluster.on("exit", (worker, code, signal) => {
+    logger.warn(
+      "worker %s died (code = %s; signal = %s). restart...",
+      worker.process.pid,
+      code,
+      signal
+    );
+    clearInterval(idCheckInterval);
+    endCheckHealth();
+    canStartCheck = true;
+    cluster.fork();
+  });
 } else {
-	const express = require('express'),
-		http = require('http'),
-		https = require('https'),
-		fs = require("fs"),
-		app = express(),
-		spellCheck  = require('./spellCheck');
-	let server = null;
+  const express = require("express"),
+    http = require("http"),
+    https = require("https"),
+    fs = require("fs"),
+    app = express(),
+    spellCheck = require("./spellCheck");
+  let server = null;
 
+  logger.warn("Express server starting...");
 
-	logger.warn('Express server starting...');
+  if (config.has("ssl")) {
+    const privateKey = fs.readFileSync(config.get("ssl.key")).toString();
+    const certificateKey = fs.readFileSync(config.get("ssl.cert")).toString();
+    const trustedCertificate = fs.readFileSync(config.get("ssl.ca")).toString();
+    //See detailed options format here: http://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
+    const options = {
+      key: privateKey,
+      cert: certificateKey,
+      ca: [trustedCertificate]
+    };
 
-	if (config.has('ssl')) {
-		const privateKey = fs.readFileSync(config.get('ssl.key')).toString();
-		const certificateKey = fs.readFileSync(config.get('ssl.cert')).toString();
-		const trustedCertificate = fs.readFileSync(config.get('ssl.ca')).toString();
-		//See detailed options format here: http://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
-		const options = {key: privateKey, cert: certificateKey, ca: [trustedCertificate]};
+    server = https.createServer(options, app);
+  } else {
+    server = http.createServer(app);
+  }
 
-		server = https.createServer(options, app);
-	} else {
-		server = http.createServer(app);
-	}
+  // Если захочется использовать 'development' и 'production',
+  // то с помощью app.settings.env (https://github.com/strongloop/express/issues/936)
+  // Если нужна обработка ошибок, то теперь она такая https://github.com/expressjs/errorhandler
+  spellCheck.install(server, function() {
+    server.listen(config.get("server.port"), function() {
+      logger.warn(
+        "Express server listening on port %d in %s mode",
+        config.get("server.port"),
+        app.settings.env
+      );
+    });
 
-	// Если захочется использовать 'development' и 'production',
-	// то с помощью app.settings.env (https://github.com/strongloop/express/issues/936)
-	// Если нужна обработка ошибок, то теперь она такая https://github.com/expressjs/errorhandler
-	spellCheck.install(server, function(){
-		server.listen(config.get('server.port'), function(){
-			logger.warn("Express server listening on port %d in %s mode", config.get('server.port'), app.settings.env);
-		});
+    app.get("/index.html", function(req, res) {
+      res.send("Server is functioning normally");
+    });
+  });
 
-		app.get('/index.html', function(req, res) {
-			res.send('Server is functioning normally');
-		});
-	});
+  process.on("message", function(msg) {
+    if (!spellCheck) return;
+    logger.info("on message %s", msg.type);
+    spellCheck.spellSuggest(msg.type, c_sCheckWord, c_sCheckLang, function(
+      res
+    ) {
+      process.send({ type: msg.type, res: res });
+    });
+  });
 
-	process.on('message', function(msg) {
-		if (!spellCheck)
-			return;
-		spellCheck.spellSuggest(msg.type, c_sCheckWord, c_sCheckLang, function(res) {
-			process.send({type: msg.type, res: res});
-		});
-	});
-
-	process.on('uncaughtException', function(err) {
-		logger.error((new Date).toUTCString() + ' uncaughtException:', err.message);
-		logger.error(err.stack);
-		logger.shutdown(function () {
-			process.exit(1);
-		});
-	});
+  process.on("uncaughtException", function(err) {
+    logger.error(new Date().toUTCString() + " uncaughtException:", err.message);
+    logger.error(err.stack);
+    logger.shutdown(function() {
+      process.exit(1);
+    });
+  });
 }
--- SpellChecker/sources/spellCheck.js
+++ SpellChecker/sources/spellCheck.js
@@ -30,119 +30,140 @@
  *
  */
 
-'use strict';
+"use strict";
 
-const sockjs = require('sockjs');
-const nodehun = require('nodehun');
-const logger = require('./../../Common/sources/logger');
-const utils = require('./../../Common/sources/utils');
-const fs = require('fs');
-const co = require('co');
-const cfgSockjs = require('config').get('services.CoAuthoring.sockjs');
-const languages = require('./languages');
+const sockjs = require("sockjs");
+const nodehun = require("nodehun");
+const logger = require("./../../Common/sources/logger");
+const utils = require("./../../Common/sources/utils");
+const fs = require("fs");
+const co = require("co");
+const cfgSockjs = require("config").get("services.CoAuthoring.sockjs");
+const languages = require("./languages");
 const allLanguages = languages.allLanguages;
-const path = require('path');
+const path = require("path");
 const arrExistDictionaries = {};
 const pathDictionaries = require('config').get('SpellChecker.server.dictDir');
 const arrDictionaries = {};
 
 function spell(type, word, id) {
-	return new Promise(function(resolve, reject) {
-		let dict = null;
-		if (arrDictionaries[id]) {
-			dict = arrDictionaries[id];
-		} else {
-			if (arrExistDictionaries[id]) {
-				let pathTmp = path.join(pathDictionaries, allLanguages[id], allLanguages[id] + '.');
-				dict = arrDictionaries[id] = new nodehun(pathTmp + 'aff', pathTmp + 'dic');
-			}
-		}
+  return new Promise(function(resolve, reject) {
+    let dict = null;
+    if (arrDictionaries[id]) {
+      dict = arrDictionaries[id];
+    } else {
+      if (arrExistDictionaries[id]) {
+        let pathTmp = path.join(
+          pathDictionaries,
+          allLanguages[id],
+          allLanguages[id] + "."
+        );
+        dict = arrDictionaries[id] = new nodehun(
+          pathTmp + "aff",
+          pathTmp + "dic"
+        );
+      }
+    }
 
-		if (dict) {
-			if ("spell" === type) {
-				// use setImmediate because https://github.com/nodejs/node/issues/5691
-				dict.isCorrect(word, function (err, correct, origWord) {
-					return setImmediate(resolve, !err && correct);
-				});
-			} else if ("suggest" === type) {
-				dict.spellSuggestions(word, function (err, correct, suggestions, origWord) {
-					return setImmediate(resolve, suggestions);
-				});
-			}
-		} else {
-			return setImmediate(resolve, true);
-		}
-	});
+    if (dict) {
+      if ("spell" === type) {
+        // use setImmediate because https://github.com/nodejs/node/issues/5691
+        dict.isCorrect(word, function(err, correct, origWord) {
+          return setImmediate(resolve, !err && correct);
+        });
+      } else if ("suggest" === type) {
+        dict.spellSuggestions(word, function(
+          err,
+          correct,
+          suggestions,
+          origWord
+        ) {
+          return setImmediate(resolve, suggestions);
+        });
+      }
+    } else {
+      return setImmediate(resolve, true);
+    }
+  });
 }
- 
-exports.install = function (server, callbackFunction) {
-	'use strict';
 
-	utils.listFolders(pathDictionaries, true).then((values) => {
-		return co(function*() {
-			let lang;
-			for (let i = 0; i < values.length; ++i) {
-				lang = languages.sToId(path.basename(values[i]));
-				if (-1 !== lang) {
-					arrExistDictionaries[lang] = 1;
-				}
-			}
-			yield spell('spell', 'color', 0x0409);
-			callbackFunction();
-		});
-	});
+exports.install = function(server, callbackFunction) {
+  "use strict";
 
-	const sockjs_echo = sockjs.createServer(cfgSockjs);
+  utils.listFolders(pathDictionaries, true).then(values => {
+    return co(function*() {
+      let lang;
+      for (let i = 0; i < values.length; ++i) {
+        lang = languages.sToId(path.basename(values[i]));
+        if (-1 !== lang) {
+          arrExistDictionaries[lang] = 1;
+        }
+      }
+      yield spell("spell", "color", 0x0409);
+      callbackFunction();
+    });
+  });
 
-	sockjs_echo.on('connection', function (conn) {
-		if (!conn) {
-			logger.error ("null == conn");
-			return;
-		}
-		conn.on('data', function (message) {
-			try {
-				let data = JSON.parse(message);
-				switch (data.type) {
-					case 'spellCheck':	spellCheck(conn, data.spellCheckData);break;
-				}
-			} catch (e) {
-				logger.error("error receiving response: %s", e);
-			}
-		});
-		conn.on('error', function () {
-			logger.error("On error");
-		});
-		conn.on('close', function () {
-			logger.info("Connection closed or timed out");
-		});
+  const sockjs_echo = sockjs.createServer(cfgSockjs);
 
-		sendData(conn, {type: 'init', languages: Object.keys(arrExistDictionaries)});
-	});
+  sockjs_echo.on("connection", function(conn) {
+    if (!conn) {
+      logger.error("null == conn");
+      return;
+    }
+    conn.on("data", function(message) {
+      logger.info("sockjs message %s", message);
+      try {
+        let data = JSON.parse(message);
+        switch (data.type) {
+          case "spellCheck":
+            spellCheck(conn, data.spellCheckData);
+            break;
+        }
+      } catch (e) {
+        logger.error("error receiving response: %s", e);
+      }
+    });
+    conn.on("error", function() {
+      logger.error("On error");
+    });
+    conn.on("close", function() {
+      logger.info("Connection closed or timed out");
+    });
 
-	function sendData(conn, data) {
-		conn.write(JSON.stringify(data));
-	}
+    sendData(conn, {
+      type: "init",
+      languages: Object.keys(arrExistDictionaries).concat(["2052"])
+    });
+  });
 
-	function spellCheck(conn, data) {
-		return co(function*() {
-			let promises = [];
-			for (let i = 0, length = data.usrWords.length; i < length; ++i) {
-				promises.push(spell(data.type, data.usrWords[i], data.usrLang[i]));
-			}
-			yield Promise.all(promises).then(values => {
-				data[('spell' === data.type ? 'usrCorrect' : 'usrSuggest')] = values;
-			});
-			sendData(conn, {type: 'spellCheck', spellCheckData: data});
-		});
-	}
+  function sendData(conn, data) {
+    conn.write(JSON.stringify(data));
+  }
 
-	sockjs_echo.installHandlers(server, {prefix:'/doc/[0-9-.a-zA-Z_=]*/c', log:function (severity, message) {
-		//TODO: handle severity
-		logger.info(message);
-	}});
+  function spellCheck(conn, data) {
+    return co(function*() {
+      let promises = [];
+      for (let i = 0, length = data.usrWords.length; i < length; ++i) {
+        promises.push(spell(data.type, data.usrWords[i], data.usrLang[i]));
+      }
+      yield Promise.all(promises).then(values => {
+        data["spell" === data.type ? "usrCorrect" : "usrSuggest"] = values;
+      });
+      sendData(conn, { type: "spellCheck", spellCheckData: data });
+    });
+  }
+
+  sockjs_echo.installHandlers(server, {
+    prefix: "/doc/[0-9-.a-zA-Z_=]*/c",
+    log: function(severity, message) {
+      //TODO: handle severity
+      logger.info(message);
+    }
+  });
 };
-exports.spellSuggest = function (type, word, lang, callbackFunction) {
-	return co(function*() {
-		callbackFunction(yield spell(type, word, lang));
-	});
+exports.spellSuggest = function(type, word, lang, callbackFunction) {
+  return co(function*() {
+    callbackFunction(yield spell(type, word, lang));
+  });
 };
